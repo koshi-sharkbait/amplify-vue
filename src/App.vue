f634c3e90e0e869c58793f62d4b3eb6e99af13ac
EC2 Default User <ec2-user@ip-10-0-8-176.ap-northeast-1.compute.internal>
2023-06-26 08:35:12 +0000

    Add Auth function

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
-  <main>
-    <header>
-      <h2>Amplify × Vue × Vite</h2>
-      <nav class="nav">
-        <ul class="list">
-          <li><router-link class="routerLink" to="/">Home</router-link></li>
-          <li><router-link class="routerLink" to="/chat">Chat</router-link></li>
-          <li><router-link class="routerLink" to="/ai">AI</router-link></li>
-          <li><router-link class="routerLink" to="/geo">Geo</router-link></li>
-        </ul>
-      </nav>
-      <button @click="signOut" class="contact">Sign out</button>
-    </header>
-    <section>
-      <router-view/>
-    </section>
-  </main>
+  <authenticator>
+    <!-- 追加 1 -->
+    <template v-slot="{ user, signOut }">
+      <main>
+        <header>
+          <h2>Amplify × Vue × Vite</h2>
+          <nav class="nav">
+            <ul class="list">
+              <li><router-link class="routerLink" to="/">Home</router-link></li>
+              <li><router-link class="routerLink" to="/chat">Chat</router-link></li>
+              <li><router-link class="routerLink" to="/ai">AI</router-link></li>
+              <li><router-link class="routerLink" to="/geo">Geo</router-link></li>
+            </ul>
+          </nav>
+          <!-- 追加 2 -->
+          <button @click="signOut" class="contact">Sign out</button>
+        </header>
+        <section>
+          <!-- 追加 3 -->
+          <router-view :username="user.username" />
+        </section>
+      </main>
+    <!-- 追加 4 -->
+    </template>
+  </authenticator>
 </template>
 
 <style>
