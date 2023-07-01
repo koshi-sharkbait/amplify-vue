9469c2c6a30941a948567691e04d4e578e7e4f59
EC2 Default User <ec2-user@ip-10-0-8-176.ap-northeast-1.compute.internal>
2023-07-01 01:09:13 +0000

    Vuetifyの追加とそれに伴うコードの修正　

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
 <template>
   <authenticator>
-    <!-- 追加 1 -->
     <template v-slot="{ user, signOut }">
       <main>
         <header>
-          <h2>Amplify × Vue × Vite</h2>
+          <h2>Chat App</h2>
           <nav class="nav">
             <ul class="list">
               <li><router-link class="routerLink" to="/">Home</router-link></li>
               <li><router-link class="routerLink" to="/chat">Chat</router-link></li>
             </ul>
           </nav>
-          <!-- 追加 2 -->
           <button @click="signOut" class="contact">Sign out</button>
         </header>
         <section>
-          <!-- 追加 3 -->
           <router-view :username="user.username" />
         </section>
       </main>
-    <!-- 追加 4 -->
     </template>
   </authenticator>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,23 +1,25 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+
+loadFonts()
 
 // Amplify
 import { Amplify } from 'aws-amplify';
 import awsconfig from './aws-exports';
 Amplify.configure(awsconfig);
 
+
 // Amplify UI Vue
 import AmplifyVue from '@aws-amplify/ui-vue';
 import '@aws-amplify/ui-vue/styles.css';
 
-// Amplify Predictions
-import { AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
-Amplify.addPluggable(new AmazonAIPredictionsProvider());
-// ↑↑↑↑↑↑
 
 const app = createApp(App);
 app.config.productionTip = false;
 app.use(router);
+app.use(vuetify);
 app.use(AmplifyVue);
 app.mount('#app');
