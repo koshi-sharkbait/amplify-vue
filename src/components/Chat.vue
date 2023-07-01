0b83eb9046f3d36739909e2bbad5e74e765aa937
EC2 Default User <ec2-user@ip-10-0-8-176.ap-northeast-1.compute.internal>
2023-07-01 04:25:02 +0000

    チャット送信箇所のUI修正

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -10,89 +10,109 @@
         </div>
       </div>
     </div>
-    <input
-      class="message-input"
-      placeholder="Enter a message (send with Shift+Enter)"
-      v-model="content"
-      @keydown.enter.shift="sendMessage"
-    />
+
+    <v-form>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-text-field v-model="content" type="text" variant="outlined">
+              <template>
+                <v-fade-transition leave-absolute>
+                  <v-progress-circular v-if="loading" color="info" indeterminate size="24">
+                  </v-progress-circular>
+                </v-fade-transition>
+              </template>
+              <template v-slot:append>
+                <v-btn @click="sendMessage">
+                  Button
+                </v-btn>
+              </template>
+            </v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+
   </div>
+
 </template>
 
+
+
 <script>
-import { API, graphqlOperation } from '@aws-amplify/api';
-import { useAuthenticator } from '@aws-amplify/ui-vue';
-import { createMessage } from '@/graphql/mutations';
-import { listMessages } from '@/graphql/queries';
-import { onCreateMessage } from '@/graphql/subscriptions';
-import { ref, onBeforeUnmount, onUpdated } from 'vue';
+  import { API, graphqlOperation } from '@aws-amplify/api';
+  import { useAuthenticator } from '@aws-amplify/ui-vue';
+  import { createMessage } from '@/graphql/mutations';
+  import { listMessages } from '@/graphql/queries';
+  import { onCreateMessage } from '@/graphql/subscriptions';
+  import { ref, onBeforeUnmount, onUpdated } from 'vue';
 
-export default {
-  props: {
-    username: String,
-  },
-  setup(props) {
-    const messages = ref([]);
-    const content = ref('');
-    const subscription = ref({});
+  export default {
+    props: {
+      username: String,
+    },
+    setup(props) {
+      const messages = ref([]);
+      const content = ref('');
+      const subscription = ref({});
 
-    const sendMessage = async () => {
-      if (event.keyCode !== 13 || !content.value) return;
+      const sendMessage = async () => {
+        if (!content.value) return;
 
-      const message = {
-        id: new Date().getTime() + props.username,
-        content: content.value,
+        const message = {
+          id: new Date().getTime() + props.username,
+          content: content.value,
+        };
+
+        // Mutation(createMessage) の実装 ↓
+        API.graphql(graphqlOperation(createMessage, { input: message })).catch((error) => console.warn(error));
+        // ↑↑↑↑↑↑
+        content.value = '';
       };
 
-      // Mutation(createMessage) の実装 ↓
-      API.graphql(graphqlOperation(createMessage, { input: message })).catch((error) => console.warn(error));
-      // ↑↑↑↑↑↑
-      content.value = '';
-    };
+      const fetch = async () => {
+        // Query(listMessages) の実装 ↓
+        API.graphql(graphqlOperation(listMessages, { input: 100 }))
+          .then((value) => (messages.value = value.data.listMessages.items.sort((a, b) => (a.id > b.id ? 1 : -1))))
+          .catch((error) => console.warn(error));
+        // ↑↑↑↑↑↑
+      };
 
-    const fetch = async () => {
-      // Query(listMessages) の実装 ↓
-      API.graphql(graphqlOperation(listMessages, { input: 100 }))
-        .then((value) => (messages.value = value.data.listMessages.items.sort((a, b) => (a.id > b.id ? 1 : -1))))
-        .catch((error) => console.warn(error));
-      // ↑↑↑↑↑↑
-    };
+      const subscribe = async () => {
+        // Subscription(onCreateMessages) の実装 1 ↓
+        subscription.value = API.graphql(graphqlOperation(onCreateMessage)).subscribe({
+          next: (eventData) => {
+            const message = eventData.value.data.onCreateMessage;
+            messages.value = [...messages.value, message];
+          },
+          error: (error) => console.warn(error),
+        });
+        // ↑↑↑↑↑↑
+      };
 
-    const subscribe = async () => {
-      // Subscription(onCreateMessages) の実装 1 ↓
-      subscription.value = API.graphql(graphqlOperation(onCreateMessage)).subscribe({
-        next: (eventData) => {
-          const message = eventData.value.data.onCreateMessage;
-          messages.value = [...messages.value, message];
-        },
-        error: (error) => console.warn(error),
+      const scrollBottom = () => {
+        const container = document.querySelector('.message_base');
+        container.scrollTop = container.scrollHeight;
+      };
+
+      onBeforeUnmount(() => {
+        // Subscription(onCreateMessages) の実装 2 ↓
+        subscription.value.unsubscribe();
+        // ↑↑↑↑↑↑
       });
-      // ↑↑↑↑↑↑
-    };
 
-    const scrollBottom = () => {
-      const container = document.querySelector('.message_base');
-      container.scrollTop = container.scrollHeight;
-    };
+      onUpdated(() => {
+        scrollBottom();
+      });
 
-    onBeforeUnmount(() => {
-      // Subscription(onCreateMessages) の実装 2 ↓
-      subscription.value.unsubscribe();
-      // ↑↑↑↑↑↑
-    });
+      fetch();
+      subscribe();
 
-    onUpdated(() => {
-      scrollBottom();
-    });
-
-    fetch();
-    subscribe();
-
-    return {
-      messages,
-      content,
-      sendMessage,
-    };
-  },
-};
+      return {
+        messages,
+        content,
+        sendMessage,
+      };
+    },
+  };
 </script>
